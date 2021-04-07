27435641519f4a83af46e1581b462a9605d9b6c9
Gustavo Granato <gustavo.granato@hotmail.com>
2021-04-07 02:26:34 -0300

    feat: create component header and config scss in project

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../assets/img/logos/webmotors.svg'
+// import { Container } from './styles';
+
+function header() {
+  return (
+    <img src={logo} width={300} alt='Quer comprar ou vender um veículo sem sair de casa? Aqui você conta com uma experiência segura e 100% online. Acesse já!' />
+  )
+}
+
+export default header;
---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/header/header'
+// import { Container } from './styles';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <button>oi</button>
+    </div>
+  );
+}
+
+export default Home;
